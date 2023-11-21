f1638e24b6e0d85a29d2464b38983a4c56276a18
Ayush9535 <ghodkeayush22@gmail.com>
2023-11-21 14:49:24 +0530

    added

--- js files/game.js
+++ js files/game.js
@@ -1,6 +1,15 @@
+
 document.getElementById("game-quit").addEventListener("click" , ()=>{
     var ans = confirm("Do you want to quit the Game?")
     if (ans == true) {
         window.location.href = "index.html";
     } 
+})
+
+var dice = document.getElementById("dice")
+var diceNumber;
+
+dice.addEventListener("click" , ()=>{
+    diceNumber = Math.floor(Math.random()*6)
+    dice.src = `./Images/${diceNumber+1}.png`
 })
