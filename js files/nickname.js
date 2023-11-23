2731bf5135973b8796ddb83d91aaa2bb91d91f3c
Ayush9535 <ghodkeayush22@gmail.com>
2023-11-23 14:08:56 +0530

    added

--- js files/nickname.js
+++ js files/nickname.js
@@ -15,11 +15,18 @@ document.getElementById("nickname-back").addEventListener("click" ,  ()=>{
 document.getElementById("nickname-next").addEventListener("click" ,  ()=>{
     var player1 = document.getElementById("player1").value
     var player2 = document.getElementById("player2").value
+    var player1nick = document.getElementById("player1nick").value
+    var player2nick = document.getElementById("player2nick").value
 
     localStorage.setItem("player1Name" , player1)
     localStorage.setItem("player2Name" , player2)
+    localStorage.setItem("player1NickName" , player1nick)
+    localStorage.setItem("player2NickName" , player2nick)
 
     if (player1 == "" || player2 == ""){
+        alert("Enter Name for player..!!")
+    }
+    else if (player1nick == "" || player2nick == ""){
         alert("Enter Nickname for player..!!")
     }else{
         window.location.href = "color.html"
